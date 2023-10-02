d8030728972d91879f8c7b7cb274af4c4c409d3f
hpdzz <tranlehuuphuoc22502@gmail.com>
2023-10-02 16:21:55 +0700

    Functional Components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar-bg">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#home">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#news">
-                News
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+export default function Navigation() {
+  return (
+    <div>
+      <nav className="navbar-bg">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#home">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#news">
+              News
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import { Players } from "../shared/ListOfPlayers";
-export default class Player extends Component {
-  render() {
-    return (
-      <div className="container">
-        {Players.map((Player) => (
-          <div className="column" key={Player.id}>
-            <div className="card">
-              <img src={Player.img} alt="#"></img>
-              <h3>{Player.name}</h3>
-              <p className="title">{Player.club}</p>
-              <p>
-                <button>Detail</button>
-              </p>
-            </div>
+export default function Player() {
+  return (
+    <div className="container">
+      {Players.map((Player) => (
+        <div className="column" key={Player.id}>
+          <div className="card">
+            <img src={Player.img} alt="#"></img>
+            <h3>{Player.name}</h3>
+            <p className="title">{Player.club}</p>
+            <p>
+              <button>Detail</button>
+            </p>
           </div>
-        ))}
-      </div>
-    );
-  }
+        </div>
+      ))}
+    </div>
+  );
 }
